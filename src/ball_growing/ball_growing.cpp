6218669262f3caf3bfd8671d49cc787eee91d6a1
Andrew Gu <andrewg2@andrew.cmu.edu>
2020-04-23 13:25:01 -0700

    add ball growing (not working)

--- src/ball_growing/ball_growing.cpp
+++ src/ball_growing/ball_growing.cpp
@@ -1,11 +1,63 @@
 #include "../util/graph.h"
 
+#include <vector>
+#include <unordered_set>
 
-void grow_ball(Graph g, int source, float beta) {
+#define PRESENT 1
+#define NOT_PRESENT 0
 
+void grow_ball(Graph g, int source, float beta, vector<bool> &present, unordered_set<int> &ball, int &R) {
+    int r = 1;
+    float isoperimetric_num = (float) g->m; // max possible
+    unordered_set<int> boundary_vertices;
+    unordered_set<int> next_boundary_vertices;
+    boundary_vertices.insert(source);
+    int degree_sum = g->out_offsets[source+1] - g->out_offsets[source];
+    ball.clear();
+    ball.insert(source);
+    while (isoperimetric_num > beta) {
+        int next_boundary_size = 0;
+        for (const auto &vid : boundary_vertices) {
+            for (int eid = g->out_offsets[vid]; eid < g->out_offsets[vid+1]; ++eid) {
+                int nid = g->out_edge_list[eid];
+                if (!present[nid]) {
+                    continue;
+                }
+                // Unvisited
+                if (next_boundary_vertices.find(nid) != next_boundary_vertices.end()) {
+                    next_boundary_vertices.insert(nid);
+                    degree_sum += g->out_offsets[nid+1] - g->out_offsets[nid];
+                }
+                next_boundary_size++;
+            }
+        }
+        // Update present
+        // Recompute isoperimetric_num by dividing
+        isoperimetric_num = next_boundary_size / degree_sum;
+        for (const auto &vid : next_boundary_vertices) {
+            present[vid] = NOT_PRESENT;
+            ball.insert(vid);
+        }
+        boundary_vertices.swap(next_boundary_vertices);
+        next_boundary_vertices.clear()
+        r++;
+    }
+    R = r;
 }
 
-void ball_decomp_seq(Graph g, float beta) {
 
+
+void ball_decomp_seq(Graph g, float beta, vector<unordered_set<int>> &collection, vector<int> &radii) {
+    //
+    vector<bool> present(g->n, PRESENT);
+    unordered_set<int> ball;
+    for (int vid = 0; vid < g->n; ++vid) {
+        int R;
+        if (present[vid] == PRESENT) {
+          grow_ball(g, x, beta, present, ball, R);
+          collection.push_back(ball);
+          radii.push_back(R);
+          ball.clear();
+        }
+    }
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "util/graph.h"
 #include "bfs/bfs_seq.cpp"
 #include "bfs/bfs_par.cpp"
+#include "ball_growing/ball_growing.cpp"
 
 /*
  * Populate g
@@ -18,66 +19,71 @@ void inline load_graph(std::string source, Graph g) {
 }
 
 int main(int argc, char **argv) {
-      int n, m;
-      Graph g = (graph_t *) malloc(sizeof(graph_t));
-      std::string graph_in(argv[1]);
-      std::string line;
-      std::ifstream source;
-      source.open(graph_in);
-      // retrieving the number of vertices and the number of edges
-      std::getline(source, line);
-      std::istringstream iss(line);
-      iss >> n >> m;
-      std::vector<std::vector<int>> in_mapper(n + 1, std::vector<int>{});
-      // populating the out-neighbor portion of the graph
-      g->out_offsets   = (int *) calloc(n + 1, sizeof(int));
-      g->out_edge_list = (int *) calloc(m,     sizeof(int));
-      g->n = n;
-      g->m = m;
-      int v   = -1;
-      int off = 0;
-      int pos = 0;
-      int cur = -1;
-      while (std::getline(source, line)) {
-          std::istringstream iss(line);
-          iss >> cur;
-          g->out_offsets[off++] = pos;
-          while (iss >> v) {
-              g->out_edge_list[pos++] = v;
-              in_mapper[v].push_back(cur);
-          }
-      }
+    int n, m;
+    Graph g = (graph_t *) malloc(sizeof(graph_t));
+    std::string graph_in(argv[1]);
+    std::string line;
+    std::ifstream source;
+    source.open(graph_in);
+    // retrieving the number of vertices and the number of edges
+    std::getline(source, line);
+    std::istringstream iss(line);
+    iss >> n >> m;
+    std::vector<std::vector<int>> in_mapper(n + 1, std::vector<int>{});
+    // populating the out-neighbor portion of the graph
+    g->out_offsets   = (int *) calloc(n + 1, sizeof(int));
+    g->out_edge_list = (int *) calloc(m,     sizeof(int));
+    g->n = n;
+    g->m = m;
+    int v   = -1;
+    int off = 0;
+    int pos = 0;
+    int cur = -1;
+    while (std::getline(source, line)) {
+        std::istringstream iss(line);
+        iss >> cur;
+        g->out_offsets[off++] = pos;
+        while (iss >> v) {
+            g->out_edge_list[pos++] = v;
+            in_mapper[v].push_back(cur);
+        }
+    }
 
-      // populating the in-neighbor portion of the graph
-      g->in_offsets   = (int *) calloc(n + 1, sizeof(int));
-      g->in_edge_list = (int *) calloc(m,     sizeof(int));
-      off = 0;
-      pos = 0;
-      // for each vertex->in_neighbor pair
-      for (int vtx = 0; vtx < in_mapper.size(); ++vtx) {
-          g->in_offsets[off++] = pos;
-          // for each in-neighbor of the current vertex
-          for (auto &in_nbor : in_mapper[vtx]) {
-              g->in_edge_list[pos++] = in_nbor;
-          }
-      }
-      std::cout << std::endl;
-      source.close();
-      std::cout << "Succesfully Loaded Graph" << std::endl;
+    // populating the in-neighbor portion of the graph
+    g->in_offsets   = (int *) calloc(n + 1, sizeof(int));
+    g->in_edge_list = (int *) calloc(m,     sizeof(int));
+    off = 0;
+    pos = 0;
+    // for each vertex->in_neighbor pair
+    for (int vtx = 0; vtx < in_mapper.size(); ++vtx) {
+        g->in_offsets[off++] = pos;
+        // for each in-neighbor of the current vertex
+        for (auto &in_nbor : in_mapper[vtx]) {
+            g->in_edge_list[pos++] = in_nbor;
+        }
+    }
+    std::cout << std::endl;
+    source.close();
+    std::cout << "Succesfully Loaded Graph" << std::endl;
 
-      int *distances = (int *) calloc(n, sizeof(int));
-      bfs_top_down_seq(g, 0, distances);
-      for (int i = 0; i < n; ++i) {
-          std::cout << distances[i] << " ";
-      }
-      std::cout << "\n\n";
+    int *distances = (int *) calloc(n, sizeof(int));
+    bfs_top_down_seq(g, 0, distances);
+    for (int i = 0; i < n; ++i) {
+        std::cout << distances[i] << " ";
+    }
+    std::cout << "\n\n";
 
-      for (int i = 0; i < n; ++i) {
-          distances[i] = 0;
-      }
-      bfs_bottom_up_par(g, 0, distances);
-      for (int i = 0; i < n; ++i) {
-          std::cout << distances[i] << " ";
-      }
-      std::cout << std::endl;
+    for (int i = 0; i < n; ++i) {
+        distances[i] = 0;
+    }
+    bfs_bottom_up_par(g, 0, distances);
+    for (int i = 0; i < n; ++i) {
+        std::cout << distances[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Sequential Ball Growing" << std::endl;
+    vector<unordered_set<int>> collection;
+    vector<int> radii;
+    ball_decomp_seq(g, 0.75, collection, radii);
 }
